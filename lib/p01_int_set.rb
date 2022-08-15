7ee287be32904d971304e543ccfbd26d6750302c
darianchen <darianychen@gmail.com>
2022-08-15 15:42:39 -0400

    IntSet complete

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -37,24 +37,30 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
-  def insert(num)
-
+  def insert(num) #private method must be called explicitly
+    self[num] << num
   end
 
   def remove(num)
+    store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    return true if @store[num % num_buckets].include?(num) 
+    false
+
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    store[num % num_buckets]
   end
 
   def num_buckets
